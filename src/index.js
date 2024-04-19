210cfbdb4ed2d1efbb066109914bf04a6b5f7202
Vlad Dracula <vlad@tran.sylvan.ia>
2024-04-19 13:32:38 +0100

    Search functionality using api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
+function refreshWeather(response) {
+let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#city");
+
+cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+
+function searchCity(city) {
+let apiKey = "c7ab0aao28aa127c97428t4f44e0f3b8";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris")
