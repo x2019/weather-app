103d8c82b0fcd668255c62cda35b6e2033fe3655
Vlad Dracula <vlad@tran.sylvan.ia>
2024-04-29 13:18:18 +0100

    Updated weather icon

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windSpeedElement = document.querySelector("#wind-speed");
 let timeElement = document.querySelector("#time");
-let date = new Date(response.data.time * 1000)
+let date = new Date(response.data.time * 1000);
+let iconElement = document.querySelector("#icon");
+
+icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
 console.log(response.data);
 
@@ -17,6 +20,7 @@ descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 temperatureElement.innerHTML = Math.round(temperature);
+
 
 }
 
